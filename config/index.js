6a23cf45339cd6690643ada1bb3b355a7abdbf19
e-chevalier <echevalier.dev@gmail.com>
2022-03-03 23:27:53 -0300

    Add desafios intermedios

--- config/index.js
+++ config/index.js
@@ -1,7 +1,9 @@
 import 'dotenv/config'
 
 let config = {
-    port: process.env.PORT
+    port: process.env.PORT,
+    server : '127.0.0.1:27017', // REPLACE WITH YOUR OWN SERVER
+    database : 'colegio'       // REPLACE WITH YOUR OWN DB NAME
 }
 
 export { config }
